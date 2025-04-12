330c6ddd9c5bbba99c75cf6c07f3b1e45b10cc89
BaoTheParo <tanthaibao123@gmail.com>
2025-04-12 08:50:31 +0700

    fix loi

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -7,7 +7,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-
+BLUE = (0, 191, 255)
 # Năng lượng
 DEFAULT_ENERGY = 100  # Năng lượng mặc định của người chơi
 MAX_ENERGY = 100      # Năng lượng tối đa ban đầu
